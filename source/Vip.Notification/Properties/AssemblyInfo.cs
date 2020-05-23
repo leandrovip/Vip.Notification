b2f014cb7c22159b6f691667e669ac56dd9c7ad5
Leandro Ferreira <leandro@vipsolucoes.com>
2020-05-23 17:58:00 -0300

    Update nuspec file

--- source/Vip.Notification/Properties/AssemblyInfo.cs
+++ source/Vip.Notification/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Vip.Notification")]
-[assembly: AssemblyDescription("Alert notifications for desktop")]
+[assembly: AssemblyDescription("Toast alert notification for desktop")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VIP Soluções")]
 [assembly: AssemblyProduct("Vip.Notification")]
